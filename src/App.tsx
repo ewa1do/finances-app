5560e896c767b63dbe2a7ce809ac7f447706bc40
Eduardo Vera <this.eduardovera@gmail.com>
2025-01-16 13:16:40 -0400

    implementing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Dashboard from "./features/dashboard/Dashboard";
+import Balance from "./features/balance/Balance";
+import Ledger from "./features/ledger/Ledger";
+import Credit from "./features/credit/Credit";
+
+interface IRoute {
+    path: string;
+    element: JSX.Element;
+}
+
+const routes: IRoute[] = [
+    { path: "/", element: <Dashboard /> },
+    { path: "/balance", element: <Balance /> },
+    { path: "/credit", element: <Credit /> },
+    { path: "/ledger", element: <Ledger /> },
+];
+
 function App() {
     return (
-        <>
-            <h1>Hello!</h1>
-        </>
+        <BrowserRouter>
+            <Routes>
+                {routes.map((route) => {
+                    return <Route path={route.path} element={route.element} />;
+                })}
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/features/balance/Balance.tsx
@@ -0,0 +1,3 @@
+export default function Balance() {
+    return <h1>Balance</h1>;
+}
