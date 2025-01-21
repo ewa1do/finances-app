761d819592450f87d53ad328a5503775ad89006e
Eduardo Vera <this.eduardovera@gmail.com>
2025-01-21 13:38:39 -0400

    making balance cards

--- src/features/balance/Balance.tsx
+++ src/features/balance/Balance.tsx
@@ -5,6 +5,18 @@ import PieChartComponent from "./PieChartComponent";
 export default function Balance() {
     return (
         <>
+            <section className="flex gap-x-1">
+                <Card size={Sizes.extra_small} title="Ingresos" titleColor="text-[#229961]">
+                    <span className="text-8xl text-[#229961] ">757</span>
+                </Card>
+                <Card size={Sizes.extra_small} title="Gastos" titleColor="text-[#dd3543]">
+                    <span className="text-8xl text-[#dd3543]">757</span>
+                </Card>
+                <Card size={Sizes.extra_small} title="Balance" titleColor="text-[#1f86f3]">
+                    <span className="text-8xl text-[#1f86f3]">757</span>
+                </Card>
+            </section>
+
             <Card title="Ventas Semanales vs Gastos">
                 <AreaChartComponent />
             </Card>
@@ -23,6 +35,7 @@ export default function Balance() {
 }
 
 enum Sizes {
+    extra_small = "w-1/3",
     small = "w-1/2",
     full = "w-full",
 }
@@ -30,16 +43,18 @@ enum Sizes {
 function Card({
     title,
     size = Sizes.full,
+    titleColor,
     children,
 }: {
     title: string;
     size?: Sizes;
+    titleColor?: string;
     children: React.ReactNode;
 }) {
     return (
         <div className={`card bg-neutral text-neutral-content mb-8 ${size as string}`}>
             <div className="card-body items-center text-center">
-                <h2 className="card-title">{title}</h2>
+                <h2 className={`card-title ${titleColor || ""} text-3xl`}>{title}</h2>
                 {children}
             </div>
         </div>
