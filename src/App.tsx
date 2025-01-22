e4b37f0dec1b357b7b0042bbaa408561784a4ce5
Eduardo Vera <this.eduardovera@gmail.com>
2025-01-22 11:49:25 -0400

    refactor routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,6 @@
 import { BrowserRouter } from "react-router";
-import Dashboard from "./features/dashboard/Dashboard";
-import Balance from "./features/balance/Balance";
-import Ledger from "./features/ledger/Ledger";
-import Credit from "./features/credit/Credit";
-import { IRoute } from "./models/IRoute";
 import Layout from "./ui/Layout";
-
-const routes: IRoute[] = [
-    { path: "/", element: <Dashboard />, title: "Inicio" },
-    { path: "/balance", element: <Balance />, title: "Balance" },
-    { path: "/credit", element: <Credit />, title: "Abonos" },
-    {
-        path: "/ledger",
-        element: <Ledger />,
-        title: "Libros",
-        children: [
-            {
-                path: "ingresos",
-                element: <div>Ingresos</div>,
-                title: "Ingresos",
-            },
-            {
-                path: "gastos",
-                element: <div>Gastos</div>,
-                title: "Gastos",
-            },
-            {
-                path: "fiados",
-                element: <div>Fiados</div>,
-                title: "Fiados",
-            },
-        ],
-    },
-];
+import { routes } from "./services/Routes";
 
 function App() {
     return (
---
+++ src/services/Routes.tsx
@@ -0,0 +1,33 @@
+import Balance from "../features/balance/Balance";
+import Credit from "../features/credit/Credit";
+import Dashboard from "../features/dashboard/Dashboard";
+import Ledger from "../features/ledger/Ledger";
+import { IRoute } from "../models/IRoute";
+
+export const routes: IRoute[] = [
+    { path: "/", element: <Dashboard />, title: "Inicio" },
+    { path: "/balance", element: <Balance />, title: "Balance" },
+    { path: "/credit", element: <Credit />, title: "Abonos" },
+    {
+        path: "/ledger",
+        element: <Ledger />,
+        title: "Libros",
+        children: [
+            {
+                path: "ingresos",
+                element: <div>Ingresos</div>,
+                title: "Ingresos",
+            },
+            {
+                path: "gastos",
+                element: <div>Gastos</div>,
+                title: "Gastos",
+            },
+            {
+                path: "fiados",
+                element: <div>Fiados</div>,
+                title: "Fiados",
+            },
+        ],
+    },
+];
