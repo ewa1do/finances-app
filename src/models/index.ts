41843caac1e6c55886ba9cef8728c1c5fa39f84c
Eduardo Vera <this.eduardovera@gmail.com>
2025-01-23 16:11:59 -0400

    organizing models folder

---
+++ src/models/index.ts
@@ -0,0 +1,13 @@
+// Interfaces
+import { Contact } from "./interfaces/IContact";
+import { IExpenseResponse } from "./interfaces/IExpenseResponse";
+import { IRoute } from "./interfaces/IRoute";
+
+// Types and Enums
+
+import { AtoZ } from "./types/AtoZ";
+import { Sizes } from "./types/ESizes";
+
+export type { Contact, IExpenseResponse, IRoute, AtoZ };
+
+export { Sizes };
